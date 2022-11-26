cf99f5c9f25c727c8b12ca7951a355a0f2b0eba9
Alexander Stefurishin <a.stefurishin@gmail.com>
2022-11-26 10:07:27 +0300

    site generator

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # How to use
 
+## Install dependencies
 ```
 npm install
+```
+ - tested with node v16.13.2 and npm v8.4.0
 
-npm run start
+## Download mp3 files
+
+```
+npm run download
 ```
 
- - all downloaded data will be in `data/` folder
- - download runs in parallel (10 files at a time)
- - `list.json` is downloaded first, and then used to gerenate all other links
- - if asset is already present in destination, it's download is skipped
- - to refresh collection, delete `list.json`
- - tested with node 16
+- all downloaded data will be in `data/` folder
+- download runs in parallel (10 files at a time)
+- `list.json` is downloaded first, and then used to gerenate all other links
+- if asset is already present in destination, it's download is skipped
+- to refresh collection, delete old `list.json`
+- be ready to have >120GB of free space on disk
+
+## Generate convenient page to listen music
+
+```
+npm run generate
+```
+
+- page will be in `dist/index.html` will be generated
+- it will use internet site links, not your local links
--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,9 @@
 import axios, { isAxiosError } from "axios";
 import fs from "fs/promises";
 import { existsSync } from "fs";
-import { z } from "zod";
-
-const noTrackItemType = z.object({ noTrack: z.literal("Y") });
-const trackItemType = z.object({
-  set: z.object({ url: z.string(), size: z.number() }),
-});
-
-const baseItemType = z.object({
-  id: z.string(),
-
-  name: z.string(),
-  playlist_set: z.array(z.string().or(z.array(z.string()))),
-  img: z.object({ name: z.string() }),
-  date: z.string(),
-  string_date: z.string(),
-  city: z.string(),
-  quote: z.string(),
-  url: z.string(),
-  tags: z.array(z.string()),
-  soundcloud_url: z.string(),
-  web_name: z.string(),
-  similar: z
-    .record(
-      z.string(),
-      z.object({
-        id: z.string(),
-        name: z.string(),
-        date: z.string(),
-        string_date: z.string(),
-      })
-    )
-    .optional(),
-});
-
-const listType = z.array(
-  baseItemType.and(z.union([noTrackItemType, trackItemType]))
-);
+import { getList } from "./common";
 
 let globalInProgress = 0;
-
-async function getList() {
-  const listFilePath = "data/list.json";
-  if (!existsSync(listFilePath)) {
-    const resp = await axios.get(
-      "https://www.artlebedev.ru/dj/json/list.html",
-      {
-        headers: { "Accept-Encoding": "identity" },
-      }
-    );
-
-    await fs.writeFile(listFilePath, JSON.stringify(resp.data), {
-      encoding: "utf-8",
-    });
-  }
-  const fileContent = await fs.readFile(listFilePath, {
-    encoding: "utf-8",
-  });
-  return listType.parse(JSON.parse(fileContent));
-}
 
 async function getSet(name: string) {
   const filePath = `data/${name}`;
@@ -82,9 +26,11 @@ async function getSet(name: string) {
     globalInProgress--;
   } catch (error) {
     if (isAxiosError(error)) {
-      console.log(`[ERROR] ${name}`);
-      console.log(error.message);
-      console.log(error.response);
+      console.log(`[ERROR] ${name} ${error.message}`);
+      await fs.appendFile(
+        "data/errors.log",
+        `${new Date().toISOString()};${name}; ${error.message}\n`
+      );
     } else {
       throw error;
     }
